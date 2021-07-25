47a343801d4b34208a5175e0034eacb915f50051
Osama-14 <mohdosama42000@gmail.com>
2021-07-25 23:51:16 +0500

    class-assignment

--- app.js
+++ app.js
@@ -1,92 +1,92 @@
-// var itemsArray = [
-//     {
-//         name:"juice",
-//         price:50,
-//         quantity:3,
-//     },
 
-//     {
-//         name:"tea",
-//         price:30,
-//         quantity:12,
-//     },
+//  var itemsArray = [
+//      { name: "juice", price: "50", quantity: "3" },
+//      { name: "cookie", price: "30", quantity: "9" },
+//      { name: "shirt", price: "880", quantity: "1" },
+//      { name: "pen", price: "100", quantity: "2" }]
 
-//     {
-//         name:"shirt",
-//         price:500,
-//         quantity:38,
-//     },
 
-//     {
-//         name:"pant",
-//         price:235,
-//         quantity:6,
-//     }
+//      let addAll = 0;
+
+//      for(var i=0;i<itemsArray.length;i++){
+//          let add = Number(itemsArray[i].price)
+//           * Number(itemsArray[i].quantity);
+
+//          addAll = addAll + add;
+
+//          console.log(itemsArray[i].name," --- ",add);
+
+
+//      }
+
+//      console.log("total price of all items are ",addAll);
 
 
 
 
-// ]
-
-    //Question1
-
-
-// var itemsArray = [
-//     { name: "juice", price: "50", quantity: "3" },
-//     { name: "cookie", price: "30", quantity: "9" },
-//     { name: "shirt", price: "880", quantity: "1" },
-//     { name: "pen", price: "100", quantity: "2" }]
-
-
-//     let addAll = 0;
-
-//     for(var i=0;i<itemsArray.length;i++){
-//         let add = Number(itemsArray[i].price)
-//          * Number(itemsArray[i].quantity);
-
-//         addAll = addAll + add;
-
-//         console.log(itemsArray[i].name," --- ",add);
-
-
-//     }
-
-//     console.log("total price of all items are ",addAll)
-
-
-    // question2
-
-    // var obj = {
-    //     name:'osama',
-    //     email:'osama321@gmail.com',
-    //     gender:'male',
-    //     age: '18',
-    //     country: 'pakistan'
-    // }
-
-
-    // if(obj.name && obj.age && obj.country){
-    //     console.log(obj.name , "" , obj.country , '' , obj.age);
-        
-    // }
-    // else{
-    //     console.log("these are not found")
-    // }
 
 
 
-    // question3
+
+//  let test = {
+//      firstName: "Osama",
+//      email: "@gmail.com",
+//      password: "etc",
+//      age: "12",
+//      gender: "male",
+//      city: "lahore",
+//      country: "Pak"
+//  }
+
+//  console.log(test.hasOwnProperty("age"))
+//  console.log(test.hasOwnProperty("country"))
+//  console.log(test.hasOwnProperty("firstName"))
+//  console.log(test.hasOwnProperty("lastName"))
 
 
-        let record1 = new Records("won by 57runs");
-        let record2 = new Records("won by 5runs");
-        let record3 = new Records("won by 4runs");
-        let record4 = new Records("won by 16runs");
 
 
-    function Records(total) {
-        this.record1 = record1;
 
-    }
+
+//  function Person(firstName,email,password,age,gender,city,country) {
+//      this.firstName = firstName,
+//      this.email = email,
+//      this.password = password,
+//      this.age = age,
+//      this.gender = gender,
+//      this.city = city,
+//      this.country = country
+//  }
+
+// let myData = new Person("m.osama","etc@gmail.com","etc",12,"male","khi","pakistan")
+// console.log(myData.firstName);
+// console.log(myData.email);
+// console.log(myData.password);
+// console.log(myData.age);
+// console.log(myData.gender);
+// console.log(myData.city);
+// console.log(myData.country);
+
 
     
+// function Record2(name, gender, address, education, profession) {
+//     this.name = name;
+//     this.gender = gender;
+//     this.address = address;
+//     this.education = education;
+//     this.profession = profession;
+// };
+
+//  var arr = [];
+
+// function getInputValue() {
+//     var inputVal1 = document.getElementById("name").value;
+//     var inputVal2 = document.getElementById("gender").value;
+//     var inputVal3 = document.getElementById("address").value;
+//     var inputVal4 = document.getElementById("education").value;
+//     var inputVal5 = document.getElementById("profession").value;
+    
+//     var rec1 = new Record2(inputVal1, inputVal2, inputVal3, inputVal4, inputVal5);
+//     arr.push(rec1);
+//     localStorage.setItem("rec1", JSON.stringify(rec1));
+//}
