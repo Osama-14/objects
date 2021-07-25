74b35db1b9292fd74de77f4876c5a8cb0858ad6a
Osama-14 <mohdosama42000@gmail.com>
2021-07-25 07:51:12 +0500

    object

---
+++ app.js
@@ -0,0 +1,92 @@
+// var itemsArray = [
+//     {
+//         name:"juice",
+//         price:50,
+//         quantity:3,
+//     },
+
+//     {
+//         name:"tea",
+//         price:30,
+//         quantity:12,
+//     },
+
+//     {
+//         name:"shirt",
+//         price:500,
+//         quantity:38,
+//     },
+
+//     {
+//         name:"pant",
+//         price:235,
+//         quantity:6,
+//     }
+
+
+
+
+// ]
+
+    //Question1
+
+
+// var itemsArray = [
+//     { name: "juice", price: "50", quantity: "3" },
+//     { name: "cookie", price: "30", quantity: "9" },
+//     { name: "shirt", price: "880", quantity: "1" },
+//     { name: "pen", price: "100", quantity: "2" }]
+
+
+//     let addAll = 0;
+
+//     for(var i=0;i<itemsArray.length;i++){
+//         let add = Number(itemsArray[i].price)
+//          * Number(itemsArray[i].quantity);
+
+//         addAll = addAll + add;
+
+//         console.log(itemsArray[i].name," --- ",add);
+
+
+//     }
+
+//     console.log("total price of all items are ",addAll)
+
+
+    // question2
+
+    // var obj = {
+    //     name:'osama',
+    //     email:'osama321@gmail.com',
+    //     gender:'male',
+    //     age: '18',
+    //     country: 'pakistan'
+    // }
+
+
+    // if(obj.name && obj.age && obj.country){
+    //     console.log(obj.name , "" , obj.country , '' , obj.age);
+        
+    // }
+    // else{
+    //     console.log("these are not found")
+    // }
+
+
+
+    // question3
+
+
+        let record1 = new Records("won by 57runs");
+        let record2 = new Records("won by 5runs");
+        let record3 = new Records("won by 4runs");
+        let record4 = new Records("won by 16runs");
+
+
+    function Records(total) {
+        this.record1 = record1;
+
+    }
+
+    
